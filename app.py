c93f7bbd19b415f9a1c704313ecc692e47fba6e5
Pranit Bhujel <pranit.bhujel@thenovaweb.com>
2023-12-29 00:17:13 +0400

    Added the cost for 2nd year.

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
 # app.py
 import streamlit as st
 
-# Constants
 
+# functions
+def calculate_cost(layer, year):
+    if year == 1:
+        if layer == 1:
+            return round(
+                (layer_1 + added_cost_per_bar) * slider_val + (20000 * conversion_rate),
+                4,
+            )
+        if layer == 2:
+            return round(
+                (layer_2 + added_cost_per_bar) * slider_val + (20000 * conversion_rate),
+                4,
+            )
+        if layer == 3:
+            return round(
+                (layer_3 + added_cost_per_bar) * slider_val + (20000 * conversion_rate),
+                4,
+            )
+    else:
+        if layer == 1:
+            return round((layer_1 + added_cost_per_bar) * slider_val, 4)
+        if layer == 2:
+            return round((layer_2 + added_cost_per_bar) * slider_val, 4)
+        if layer == 3:
+            return round((layer_3 + added_cost_per_bar) * slider_val, 4)
+
+
+# Constants
 conversion_rate = st.sidebar.slider("**Conversion (Eur € - Dol $)**", 1.0, 1.5, 1.11)
 
 layer_1 = round(1400 * conversion_rate)
@@ -37,7 +64,7 @@ production_capacity = st.sidebar.number_input(
 
 st.sidebar.divider()
 slider_val = st.sidebar.slider(
-    "**Number of Cartridges (year)**", 1, 100, round((production_capacity / 5000) * 12)
+    "**Number of Cartridges (year)**", 1, 150, round((production_capacity / 5000) * 12)
 )
 st.sidebar.caption(f"*Annual Production:* `{slider_val*5000}`")
 
@@ -84,7 +111,7 @@ st.table(with_contract)
 
 st.divider()
 
-st.write("Without Contract (**`1 Year`**)")
+st.write("Without Contract")
 st.caption(
     f"""
 - This does not include the device & the shipping cost.
@@ -95,29 +122,47 @@ Paying for - **`{slider_val}`** cartridges, device & shipping!
 """
 )
 
-added_cost_per_bar = (20000 * conversion_rate) / (slider_val * 5000)
+# Added cost per bar for the production of the entire year
+cost_per_bar = (20000 * conversion_rate) / (slider_val * 5000)
+added_cost_per_bar = round(cost_per_bar, 1)
+
 actual_cost_l1 = (layer_1 + added_cost_per_bar) * slider_val + (20000 * conversion_rate)
 actual_cost_l2 = (layer_2 + added_cost_per_bar) * slider_val + (20000 * conversion_rate)
 actual_cost_l3 = (layer_3 + added_cost_per_bar) * slider_val + (20000 * conversion_rate)
 
-st.caption(f"Extra cost per bar: **`{round(added_cost_per_bar, 5)}`**")
+st.caption(f"Extra cost per bar: **`{round(cost_per_bar, 5)}`**")
 
 without_contract = {
     "Cost ($)": ["Per Bar", "Per Month", "Per Year"],
-    "Layer 1": [
-        round((layer_1) / 5000 + added_cost_per_bar, 4),
-        round(actual_cost_l1 / 12, 4),
-        round(actual_cost_l1, 4),
+    "Layer 1 (1st Year)": [
+        round((layer_1) / 5000 + cost_per_bar, 4),
+        calculate_cost(layer=1, year=1) // 12,
+        calculate_cost(layer=1, year=1),
     ],
-    "Layer 2": [
-        round((layer_2) / 5000 + added_cost_per_bar, 4),
-        round(actual_cost_l2 / 12, 4),
-        round(actual_cost_l2, 4),
+    "Layer 1 (2nd Year)": [
+        round((layer_1) / 5000, 4),
+        calculate_cost(layer=1, year=2) // 12,
+        calculate_cost(layer=1, year=2),
     ],
-    "Layer 3": [
-        round((layer_3) / 5000 + added_cost_per_bar, 4),
-        round(actual_cost_l3 / 12, 4),
-        round(actual_cost_l3, 4),
+    "Layer 2 (1st Year)": [
+        round((layer_2) / 5000 + cost_per_bar, 4),
+        calculate_cost(layer=2, year=1) // 12,
+        calculate_cost(layer=2, year=1),
+    ],
+    "Layer 2 (2nd Year)": [
+        round((layer_2) / 5000, 4),
+        calculate_cost(layer=2, year=2) // 12,
+        calculate_cost(layer=2, year=2),
+    ],
+    "Layer 3 (1st Year)": [
+        round((layer_3) / 5000 + cost_per_bar, 4),
+        calculate_cost(layer=3, year=1) // 12,
+        calculate_cost(layer=3, year=1),
+    ],
+    "Layer 3 (2nd Year)": [
+        round((layer_2) / 5000, 4),
+        calculate_cost(layer=3, year=2) // 12,
+        calculate_cost(layer=3, year=2),
     ],
 }
 st.table(without_contract)
